a2cf2d3f79cad066b7ab5d3b002d038148d1e7fe
EashanVytla <Eashan.Vytla@gmail.com>
2025-03-10 12:19:40 -0400

    Working on raspi:

--- containers/raspi/launch.sh
+++ containers/raspi/launch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# filepath: /home/eashan/DreamingFalconROS/containers/jetson/build_and_run.sh
 
 # Terminal colors
 GREEN='\033[0;32m'
@@ -7,15 +6,17 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Navigate to workspace
-cd /ros2_ws
+cd /ros2_ws/DreamingFalconROS
 
 # Build packages
 echo -e "${YELLOW}Building ROS packages...${NC}"
-colcon build --symlink-install
+colcon build
 
 # Source workspace
-source /ros2_ws/DreamingFalconROS/install/setup.bash
+source /ros2_ws/DreamingFalconROS/install/local_setup.bash
 
 # Launch application
 echo -e "${GREEN}Build complete. Starting ROS application...${NC}"
-ros2 launch dreaming_falcon px4_ros_com_launch.py
+pwd
+ls /ros2_ws/DreamingFalconROS/install/px4_ros_com/lib/px4_ros_com
+ros2 launch px4_ros_com chirp.launch.py
--- containers/raspi/run_docker.sh
+++ containers/raspi/run_docker.sh
@@ -5,10 +5,13 @@ CONTAINER_NAME="dreamer_container"
 HOST_DIR="/home/eashan/ros2_ws"
 CONTAINER_SRC_DIR="/ros2_ws"
 
+docker rm $CONTAINER_NAME
+
 docker run -it \
     --name "$CONTAINER_NAME" \
     --network host \
     --privileged \
     -v "$HOST_DIR":"$CONTAINER_SRC_DIR" \
     "$IMAGE_NAME" \
-    /ros2_ws/DreamingFalconROS/containers/jetson/launch.sh
+    /ros2_ws/DreamingFalconROS/containers/raspi/launch.sh
+
